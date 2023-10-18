95f553fe536f7525b1d287cad2de0f3be877aef2
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com.>
2023-10-18 23:43:26 +0530

    get data from server side

--- src/client.ts
+++ src/client.ts
@@ -4,22 +4,22 @@ const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL || "";
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY || "";
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
-// const tableName: string = "search_queries";
+const tableName: string = "search_queries";
+const col_name: string = "search_query";
 
-console.log({ supabase, supabaseKey, supabaseUrl });
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function getAllSearchQueries(): Promise<any> {
+async function getAllSearchQueries(): Promise<string[]> {
   try {
-    const data = await supabase.from("search_queries").select("*");
-
-    // console.log(data);
-    return data || [];
+    const { data, error } = await supabase.from(tableName).select(col_name);
+    if (error) throw error;
+    if (data) {
+      return data || [];
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     console.error("Error fetching search queries:", error.message);
     return [];
   }
+  return [];
 }
 
 export { getAllSearchQueries };
