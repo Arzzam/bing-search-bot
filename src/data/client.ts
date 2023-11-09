7ecb02e97f4e01db0aa60315eba92d494c51567b
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com.>
2023-11-09 22:04:24 +0530

    update in UI and error handling

--- src/data/client.ts
+++ src/data/client.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY || "";
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY || '';
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
-const tableName: string = "search_queries";
-const col_name: string = "search_query";
+const tableName: string = 'search_queries';
+const col_name: string = 'search_query';
 
 async function getAllSearchQueries(): Promise<any[]> {
   try {
@@ -16,7 +16,7 @@ async function getAllSearchQueries(): Promise<any[]> {
       return data || [];
     }
   } catch (error: any) {
-    console.error("Error fetching search queries:", error.message);
+    console.error('Error fetching search queries:', error.message);
     return [];
   }
   return [];
