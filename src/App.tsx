6d5186286ee14c92ddb6a72ee6b053410f5a6eb3
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com.>
2023-12-02 13:26:15 +0530

    update search logic and stop logic and add instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { getAllSearchQueries } from './data/client';
 import Input from './components/Input';
 import Button from './components/Button';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ModalContainer from './components/Modal';
 
 function App() {
   const [numSearches, setNumSearches] = useState<string>('10');
-  const [delayTime, setDelayTime] = useState<string>('5');
+  // const [delayTime, setDelayTime] = useState<string>('5');
   const [data, setData] = useState<string[]>([]);
   const [searchQueryWindowSet, setSearchQueryWindowSet] = useState<Set<Window>>(
     new Set<Window>()
@@ -16,6 +17,7 @@ function App() {
   const [searchQuerySet, setSearchQuerySet] = useState<Set<string>>(
     new Set<string>()
   );
+  const isSearchingRef = useRef<boolean>(false);
 
   async function fetchSearchQueries() {
     const searchQueriesObject = await getAllSearchQueries();
@@ -29,6 +31,11 @@ function App() {
     fetchSearchQueries();
   }, []);
 
+  const simulateSearch = async () => {
+    const delay = Math.floor(Math.random() * 5000) + 5000; // Random delay between 5 to 10 seconds
+    return new Promise((resolve) => setTimeout(resolve, delay));
+  };
+
   function getRandomSearchQuery(): string {
     const randomIndex = Math.floor(Math.random() * data.length + 1);
     if (searchQuerySet.has(data[randomIndex])) {
@@ -38,57 +45,39 @@ function App() {
     }
   }
 
-  let searchTimeouts: NodeJS.Timeout[] = [];
-
-  const scheduleSearches = () => {
-    if (!numSearches || !delayTime) {
+  const scheduleSearches = async () => {
+    if (!numSearches) {
       toast.info('Please enter both number of searches and delay time.');
       return;
     }
-
-    searchTimeouts.forEach((timeoutId) => {
-      clearTimeout(timeoutId);
-    });
-
     if (data && data.length > 0) {
-      const numSearchesInt: number = parseInt(numSearches, 10);
-      const delayTimeInt: number = parseInt(delayTime, 10);
+      const numSearchesInt = parseInt(numSearches, 10);
+      // const delayTimeInt = parseInt(delayTime, 10);
+      isSearchingRef.current = true;
       toast(
-        `Bing searches has been initiated with ${numSearchesInt} searches.`
+        `Bing searches have been initiated with ${numSearchesInt} searches.`
       );
 
       for (let i = 0; i < numSearchesInt; i++) {
-        const timeoutId = setTimeout(() => {
-          const searchQuery = getRandomSearchQuery();
-          const searchUrl = `https://www.bing.com/search?pglt=2083&q=${encodeURIComponent(
-            searchQuery
-          )}&FORM=ANNTA1&PC=U531`;
+        if (!isSearchingRef.current) {
+          break;
+        }
+        const searchQuery = getRandomSearchQuery();
+        await simulateSearch();
+        const searchUrl = `https://www.bing.com/search?pglt=2083&q=${encodeURIComponent(
+          searchQuery
+        )}&FORM=ANNTA1&PC=U531`;
 
-          const newTab = window.open(searchUrl, '_blank');
-          if (newTab) {
-            setSearchQueryWindowSet((prev) => prev.add(newTab as Window));
-            setSearchQuerySet((prev) => prev.add(searchUrl));
-          }
-        }, i * delayTimeInt * 1000);
-        searchTimeouts.push(timeoutId);
+        const newTab = window.open(searchUrl, '_blank');
+        if (newTab) {
+          setSearchQueryWindowSet((prev) => prev.add(newTab as Window));
+          setSearchQuerySet((prev) => prev.add(searchUrl));
+        }
       }
+      isSearchingRef.current = false;
     } else {
       toast.error('No search queries found. Error in fetching data.');
     }
-  };
-
-  const stopSearches = () => {
-    if (searchTimeouts.length === 0) {
-      toast('No searches are running.');
-      return;
-    }
-    if (searchTimeouts.length > 0) {
-      toast('Stopping Searches');
-    }
-    searchTimeouts.forEach((timeoutId) => {
-      clearTimeout(timeoutId);
-    });
-    searchTimeouts = [];
   };
 
   const handleNumSearchesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -103,17 +92,32 @@ function App() {
     }
   };
 
-  const handleDelayTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value === '') {
-      setDelayTime('');
-    } else {
-      const num = parseInt(value, 10);
-      if (num >= 4 && num <= 20) {
-        setDelayTime(value);
-      }
+  const stopSearches = () => {
+    if (searchQueryWindowSet.size < 1) {
+      toast('No searches are running.');
+      return;
     }
+    if (searchQueryWindowSet.size > 0) {
+      toast('Upcoming all searches have been stopped.');
+      isSearchingRef.current = false;
+    }
+    searchQueryWindowSet.forEach((searchQuery) => {
+      searchQuery.close();
+    });
+    setSearchQueryWindowSet(new Set<Window>());
   };
+
+  // const handleDelayTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = e.target.value;
+  //   if (value === '') {
+  //     setDelayTime('');
+  //   } else {
+  //     const num = parseInt(value, 10);
+  //     if (num >= 4 && num <= 20) {
+  //       setDelayTime(value);
+  //     }
+  //   }
+  // };
 
   const closeAllTabsHandler = () => {
     if (searchQueryWindowSet.size < 1) {
@@ -146,7 +150,7 @@ function App() {
           onChange={handleNumSearchesChange}
           required
         />
-        <Input
+        {/* <Input
           label="Delay Time (in seconds):"
           id="delayTime"
           value={delayTime}
@@ -154,12 +158,15 @@ function App() {
           max={20}
           onChange={handleDelayTimeChange}
           required
-        />
+        /> */}
         <div className="flex justify-between gap-2 flex-row">
-          <Button onClick={scheduleSearches}>Start Searches</Button>
+          <Button onClick={scheduleSearches} disabled={isSearchingRef.current}>
+            Start Searches
+          </Button>
           <Button onClick={stopSearches}>Stop Searches</Button>
         </div>
         <Button onClick={closeAllTabsHandler}>Close all Tabs</Button>
+        <ModalContainer />
       </div>
       <Analytics />
     </>
