beecad67e115a6373a26bbac83101f89d38cb81d
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2025-05-26 23:31:05 +0530

    add footer and fix input range and color

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import ModalContainer from './components/Modal';
 
 function App() {
   const [numSearches, setNumSearches] = useState<string>('10');
-  // const [delayTime, setDelayTime] = useState<string>('5');
   const [data, setData] = useState<string[]>([]);
   const [searchQueryWindowSet, setSearchQueryWindowSet] = useState<Set<Window>>(
     new Set<Window>()
@@ -88,7 +87,7 @@ function App() {
       setNumSearches('');
     } else {
       const num = parseInt(value, 10);
-      if (num >= 1 && num <= 30) {
+      if (num >= 1 && num <= 50) {
         setNumSearches(value);
       }
     }
@@ -104,18 +103,6 @@ function App() {
       toast('Upcoming all searches have been stopped.');
     }
   };
-
-  // const handleDelayTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const value = e.target.value;
-  //   if (value === '') {
-  //     setDelayTime('');
-  //   } else {
-  //     const num = parseInt(value, 10);
-  //     if (num >= 4 && num <= 20) {
-  //       setDelayTime(value);
-  //     }
-  //   }
-  // };
 
   const closeAllTabsHandler = () => {
     if (searchQueryWindowSet.size < 1) {
@@ -133,7 +120,7 @@ function App() {
 
   return (
     <>
-      <div className="font-sans flex flex-col items-center justify-center h-screen gap-4">
+      <div className="font-sans flex flex-col items-center justify-center h-[calc(100vh-56px)] gap-4">
         <ToastContainer />
         <h1 className="text-center text-4xl font-bold text-orange-300">
           Bing Search Bot
@@ -146,17 +133,7 @@ function App() {
           min={1}
           max={50}
           onChange={handleNumSearchesChange}
-          required
         />
-        {/* <Input
-          label="Delay Time (in seconds):"
-          id="delayTime"
-          value={delayTime}
-          min={5}
-          max={20}
-          onChange={handleDelayTimeChange}
-          required
-        /> */}
         <div className="flex justify-between gap-2 flex-row">
           <Button onClick={scheduleSearches} disabled={isSearchingRef.current}>
             Start Searches
@@ -166,6 +143,35 @@ function App() {
         <Button onClick={closeAllTabsHandler}>Close all Tabs</Button>
         <ModalContainer />
       </div>
+      <footer className="text-center text-sm text-white flex flex-col justify-center items-center p-2">
+        <span>
+          Made with ❤️ by{' '}
+          <span
+            className="text-orange-300 cursor-pointer hover:text-orange-400"
+            onClick={() => window.open('https://github.com/Arzzam', '_blank')}
+          >
+            Arzzam
+          </span>
+        </span>
+        <div className="cursor-pointer flex items-center justify-center" >
+          <span className="text-orange-300">Give a ⭐ on GitHub if you liked it!</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className='ml-1 text-white hover:text-orange-400 w-4 h-4'
+          >
+            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+            <path d="M9 18c-4.51 2-5-2-7-2" />
+          </svg>
+        </div>
+      </footer>
       <Analytics />
     </>
   );
