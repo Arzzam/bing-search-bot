f2f8f68a980f0f75551cd007a134cf047e1b93d2
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2025-05-26 23:37:54 +0530

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,7 @@ function App() {
             Arzzam
           </span>
         </span>
-        <div className="cursor-pointer flex items-center justify-center" >
+        <div className="flex items-center justify-center" >
           <span className="text-orange-300">Give a ⭐ on GitHub if you liked it!</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -165,7 +165,7 @@ function App() {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            className='ml-1 text-white hover:text-orange-400 w-4 h-4'
+            className='ml-1 text-white hover:text-orange-400 w-4 h-4 cursor-pointer'
           >
             <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
             <path d="M9 18c-4.51 2-5-2-7-2" />
